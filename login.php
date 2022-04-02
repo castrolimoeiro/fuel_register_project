01fa8b40b2cbdd372988a0dcff87063d030501d1
David Castro <castrolimoeiro@gmail.com>
2022-04-02 09:29:23 -0300

    version_1

--- login.php
+++ login.php
@@ -1,3 +1,10 @@
+<?php
+// botão enviar
+if(isset($_POST['btn-entrar'])):
+    echo "clicou";
+endif;
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,8 +14,8 @@
     <title>Login</title>
 </head>
 <body>
-    <h1>Login</h1>
-    <form action="" method="POST">
+    <h1> Login </h1>
+    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
         Login: <input type="text" name="login"><br>
         Senha: <input type="password" name="senha"><br><br>
     <button type="submit" name="btn-entrar">Entrar</button>
